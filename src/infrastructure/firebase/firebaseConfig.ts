74e72dc521ab59d537c3abb406d3bf6f6904dc23
scifoxzdev <nonder1412@gmail.com>
2026-02-04 22:54:13 +0700

    feat: update Firebase configuration to use environment variables

--- src/infrastructure/firebase/firebaseConfig.ts
+++ src/infrastructure/firebase/firebaseConfig.ts
@@ -4,13 +4,13 @@ import { initializeApp, FirebaseApp } from 'firebase/app';
 import { getFirestore, Firestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCxIwZX7UUmXligxQ4z0g8TGCcvJUZU4pU',
-  authDomain: 'waitingtime-15bd1.firebaseapp.com',
-  projectId: 'waitingtime-15bd1',
-  storageBucket: 'waitingtime-15bd1.firebasestorage.app',
-  messagingSenderId: '219534942607',
-  appId: '1:219534942607:web:3fbece1a9f6880a3d4ebfa',
-  measurementId: 'G-ZYX157G5WB',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 let app: FirebaseApp | null = null;
